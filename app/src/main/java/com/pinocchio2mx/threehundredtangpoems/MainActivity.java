8819ca8e8f783a8df85e6e5b128403f2bebeb465
pinocchio2mx <pinocchio2mx@qq.com>
2016-10-26 22:19:34 +0800

    add splash page

--- app/src/main/java/com/pinocchio2mx/threehundredtangpoems/MainActivity.java
+++ app/src/main/java/com/pinocchio2mx/threehundredtangpoems/MainActivity.java
@@ -2,12 +2,24 @@ package com.pinocchio2mx.threehundredtangpoems;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements PoemListFragment.OnPoemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPoemSelected(int PoemID) {
+
     }
 }
---
+++ app/src/main/java/com/pinocchio2mx/threehundredtangpoems/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.pinocchio2mx.threehundredtangpoems;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    private static  final int Delay_MILLIS = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        }, Delay_MILLIS);
+    }
+}
